f5235b37948feb4f3b306ccafa589851e6ea1e05
wihanza <wihanmarais@sun.ac.za>
2024-09-21 23:19:18 +0200

    Update create_invoice.R

--- R/create_invoice.R
+++ R/create_invoice.R
@@ -2,21 +2,21 @@
 #'
 #' Create (and optionally edit) a draft of the `invoicer` R Markdown template.
 #'
-#' @param reference Invoice reference number
+#' @param path File path for invoice `.Rmd` destination
 #' @param create_dir TRUE to create a new directory for the draft
 #' @param edit TRUE to edit the template immediately
 #'
 #' @return The contents of the `invoicer` template produced at specified `file`
 #' @export
 
-create_invoice <- function(reference = "Invoice.Rmd", create_dir = FALSE, edit = FALSE) {
-  file <- paste(reference, "Invoice.Rmd")
+create_invoice <- function(path, create_dir = FALSE, edit = FALSE) {
 
   rmarkdown::draft(
-    file = file,
+    file = path,
     template = "invoicer",
     package = "wihantemplates",
     edit = edit,
     create_dir = create_dir
   )
+
 }
