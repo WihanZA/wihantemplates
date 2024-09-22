a489afb318fab68d22800d54a90b2889157e8ffe
wihanza <wihanmarais@sun.ac.za>
2024-09-22 11:58:07 +0200

    add overwrite argument to create_invoice

--- R/create_invoice.R
+++ R/create_invoice.R
@@ -3,13 +3,22 @@
 #' Create (and optionally edit) a draft of the `invoicer` R Markdown template.
 #'
 #' @param path File path for invoice `.Rmd` destination
+#' @param overwrite TRUE to overwrite an existing file at `path`
 #' @param create_dir TRUE to create a new directory for the draft
 #' @param edit TRUE to edit the template immediately
 #'
 #' @return The contents of the `invoicer` template produced at specified `file`
 #' @export
 
-create_invoice <- function(path, create_dir = FALSE, edit = FALSE) {
+create_invoice <- function(path, overwrite = FALSE, create_dir = FALSE, edit = FALSE) {
+  if (file.exists(path)) {
+    if (overwrite == TRUE) {
+      file.remove(path)
+    } else {
+      stop("File already exists. Set `overwrite = TRUE` to overwrite.")
+    }
+  }
+
   rmarkdown::draft(
     file = path,
     template = "invoicer",
